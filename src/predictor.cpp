b695f47f519c61317a60e10fe0025ffdaa7edae3
Venkateshwaran Sivaramakrishnan <vsivaramakrishnan@ucsd.edu>
2025-02-09 16:29:52 -0800

    Tage Predictor

--- src/predictor.cpp
+++ src/predictor.cpp
@@ -8,6 +8,16 @@
 #include <stdio.h>
 #include <math.h>
 #include "predictor.h"
+#include <bitset>
+#include <execinfo.h>
+#include <signal.h>
+#include <exception>
+#include <iostream>
+#include <csignal>
+#include <cstdlib>
+#include <unistd.h>
+
+using namespace std;
 
 //
 // TODO:Student Information
@@ -50,6 +60,63 @@ int tghistoryBits = 17; // Number of bits used for Global History
 int tlhistoryBits = 10; // Number of bits used for Local History
 int phtIndexBits = 10;
 
+// custom: tagei
+#define NUM_TAGE_TABLES 12
+#define TAGE_MAX 7 			// 3-bit Tage Predictor
+#define BIMODAL_SIZE 13
+#define BHT_INIT 2 			// Because loops are initially biased to be Taken
+#define BHT_MAX 3			// 2-bit Bimodal Predictor
+#define GHIST_MAX_LEN 641
+#define GHIST bitset<GHIST_MAX_LEN>
+#define USE_ALT_ON_NA_INIT 8
+#define USE_ALT_ON_NA_MAX 15
+#define WEAK_TAKEN 4
+#define WEAK_NOT_TAKEN 3
+#define UBIT_MAX 3
+#define MAX_INSTR_COUNT 18 // 256K Instruction: (1 << 18) yields 262144 instructions
+#define PHR_SIZE 16
+
+typedef struct tage {
+  uint16_t tag;
+  uint16_t ctr;
+  uint16_t u;
+} tage_t;
+
+typedef struct prediction {
+  bool pred;
+  uint32_t table;
+  uint32_t index;
+  bool alterPred;
+  uint32_t alterTable;
+  uint32_t alterIndex;
+} prediction_t;
+
+typedef struct shiftReg {
+  uint16_t data;
+  uint16_t actLength; // Actual Length
+  uint16_t newLength; 
+} cShiftReg_t;
+
+GHIST GHR;
+uint8_t *BHT;
+uint32_t PHR;
+prediction_t predict;
+uint32_t hist[NUM_TAGE_TABLES] = {640, 403, 254, 160, 101, 64, 40, 25, 16, 10, 6, 4}; // History Length from high to low
+uint32_t tableSize[NUM_TAGE_TABLES] = {9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 10, 10}; // Number of 2^n entries from high to flow
+uint32_t tagWidth[NUM_TAGE_TABLES] = {15, 14, 13, 12, 12, 11, 10, 9, 8, 8, 7, 7}; // Descending order helps in finding prediction with most history-bits first
+uint32_t *tageTableSize;
+uint32_t *tageTagSize;
+uint32_t *tageHistory;
+uint32_t bimodalEntries;
+uint32_t *tageIndex;
+uint32_t *tageTag;
+uint32_t use_alt_on_na; // Use Alternate Prediction on New Allocation
+tage_t **tageTables;
+uint64_t count;
+bool toggle;
+cShiftReg_t *csrIndex;
+cShiftReg_t **csrTag;
+
 //------------------------------------//
 //        Predictor Functions         //
 //------------------------------------//
@@ -57,6 +124,335 @@ int phtIndexBits = 10;
 // Initialize the predictor
 //
 
+// DEBUG: Stack Trace
+void signalHandler(int signal) {
+    if (signal == SIGSEGV) {
+        cerr << "Error: Segmentation fault (SIGSEGV) caught!\n";
+
+        void *array[10];
+        size_t size = backtrace(array, 10); // Capture stack trace
+        char **messages = backtrace_symbols(array, size); // Convert to symbols
+
+        cerr << "Stack trace:\n";
+        for (size_t i = 0; i < size; ++i) {
+            cerr << messages[i] << endl; // Print raw stack trace symbol
+
+            // Extract function address
+            string addr = messages[i];
+
+            // Use addr2line to get file name and line number
+            char command[256];
+            snprintf(command, sizeof(command), "addr2line -e predictor %p", array[i]);
+            system(command); // Run addr2line
+        }
+
+        free(messages); // Free memory allocated by backtrace_symbols()
+        exit(EXIT_FAILURE);
+    }
+}
+// tage functions
+void initializeFoldReg(cShiftReg_t *csr, uint32_t actLength, uint32_t newLength)
+{
+  csr->data = 0;
+  csr->actLength = actLength;
+  csr->newLength = newLength; 
+}
+
+void foldReg(cShiftReg_t* csr)
+{
+  csr->data = (csr->data << 1) | GHR[0]; // Left-shift the CSR data and append the latest GHR bit to the LSB of CSR
+  csr->data = (csr->data) ^ (GHR[csr->actLength] << (csr->actLength % csr->newLength));// Hash the GHR information at actual length with the CSR (one of many techniques from PPM)
+  csr->data = (csr->data) ^ ((csr->data & (1 << csr->newLength)) >> csr->newLength); // Preserve any overflows from the previous operation by XORing with LSB of CSR
+  csr->data = (csr->data) & ((1 << csr->newLength) - 1);
+}
+
+uint32_t calTag(uint32_t pc, uint32_t tageNum, uint32_t tageTagSize)
+{
+  uint32_t tag = (pc ^ csrTag[0][tageNum].data ^ (csrTag[1][tageNum].data << 1));
+  tag = (tag & ((1 << tageTagSize) - 1));
+  return tag;
+}
+
+uint32_t calIndex(uint32_t pc, uint32_t tageNum, uint32_t tageTagSize)
+{
+  uint32_t index = pc ^ (pc >> tageTagSize) ^ csrIndex[tageNum].data ^ PHR;
+  index = (index & ((1 << tageTagSize) - 1));
+  return index;
+}
+
+void init_tage()
+{
+  GHR.reset(); // Set all bits of GHR to 0
+  PHR = 0;
+  bimodalEntries = 1 << BIMODAL_SIZE;
+  predict.pred = -1;
+  predict.alterPred = -1;
+  predict.table = NUM_TAGE_TABLES;
+  predict.alterTable = NUM_TAGE_TABLES;
+  use_alt_on_na = USE_ALT_ON_NA_INIT;
+  count = 0;
+  toggle = 0;
+
+  BHT = (uint8_t *)malloc(bimodalEntries * sizeof(uint8_t));
+  tageTableSize = (uint32_t *)malloc(NUM_TAGE_TABLES * sizeof(uint32_t));
+  tageTagSize = (uint32_t *)malloc(NUM_TAGE_TABLES * sizeof(uint32_t));
+  tageHistory = (uint32_t *)malloc(NUM_TAGE_TABLES * sizeof(uint32_t));
+  tageIndex = (uint32_t *)malloc(NUM_TAGE_TABLES * sizeof(uint32_t));
+  tageTag = (uint32_t *)malloc(NUM_TAGE_TABLES * sizeof(uint32_t));
+  csrIndex = (cShiftReg_t *)malloc(NUM_TAGE_TABLES * sizeof(cShiftReg_t));
+  tageTables = new tage_t*[NUM_TAGE_TABLES];
+  csrTag = new cShiftReg_t*[2];
+  csrTag[0] = new cShiftReg_t[NUM_TAGE_TABLES]; 
+  csrTag[1] = new cShiftReg_t[NUM_TAGE_TABLES];
+  uint32_t tempSize;
+
+  for(int i = 0; i < NUM_TAGE_TABLES; i++)
+  {
+    tageTableSize[i] = tableSize[i];
+    tageTagSize[i] = tagWidth[i];
+    tageHistory[i] = hist[i];
+    tageIndex[i] = 0;
+    tageTag[i] = 0;
+     
+	
+    tempSize = 1 << tageTableSize[i];
+    tageTables[i] = new tage_t[tempSize];
+    for(uint32_t j = 0; j < tempSize; j++)
+    {
+      tageTables[i][j].tag = 0;
+      tageTables[i][j].ctr = 0;
+      tageTables[i][j].u = 0;
+
+    }
+
+    // Create Circular Shift Registers for folding the Global History Register
+    initializeFoldReg(&csrIndex[i], tageTagSize[i], tageHistory[i]);
+    initializeFoldReg(&csrTag[0][i], tageTagSize[i], tageHistory[i]);
+    initializeFoldReg(&csrTag[1][i], tageTagSize[i]-1, tageHistory[i]);
+
+  }
+
+  for(uint32_t i = 0; i < bimodalEntries; i++)
+  {
+    BHT[i] = BHT_INIT; 
+  }
+
+}
+
+uint8_t tage_predict(uint32_t pc)
+{
+	signal(SIGSEGV, signalHandler);
+  // get lower ghistoryBits of pc
+  uint32_t bimodalIndex = (pc & ((1 << BIMODAL_SIZE) - 1));
+  predict.pred = -1;
+  predict.alterPred = -1;
+  predict.table = NUM_TAGE_TABLES;
+  predict.alterTable = NUM_TAGE_TABLES;
+  
+  for(int i = 0; i < NUM_TAGE_TABLES; i++)
+  {
+    tageIndex[i] = calIndex(pc, i, tageTableSize[i]);
+    tageTag[i] = calTag(pc, i, tageTagSize[i]); 
+  }
+  
+  // Check for Tag hits in the Tage Tables
+  for(int i = 0; i < NUM_TAGE_TABLES; i++)
+  {
+    if(tageTables[i][tageIndex[i]].tag == tageTag[i])
+    {
+      predict.table = i;
+      predict.index = tageIndex[i]; 
+      break;
+    }
+  }
+  
+  // Check for Tag hits in the Alternate Tage Tables starting from predicted + 1 index
+  for(int i = predict.table + 1; i < NUM_TAGE_TABLES; i++)
+  {
+    if(tageTables[i][tageIndex[i]].tag == tageTag[i])
+    {
+      predict.alterTable = i;
+      predict.alterIndex = tageIndex[i]; 
+      break;
+    }
+  }
+
+  // Prediction Logic
+  if(predict.table < NUM_TAGE_TABLES) // Indicates hit in Tage Table
+  {
+    if(predict.alterTable == NUM_TAGE_TABLES) // Indicates miss for Alternative Tage Table lookup
+    {
+      predict.alterPred = (BHT[bimodalIndex] > BHT_MAX/2); // Bimodal prediction becomes the Alternative prediction
+    }
+    else
+    {
+      if(tageTables[predict.alterTable][predict.alterIndex].ctr > TAGE_MAX/2) predict.alterPred = TAKEN;
+      else predict.alterPred = NOTTAKEN;
+    }
+    if(tageTables[predict.table][predict.index].ctr != WEAK_TAKEN || tageTables[predict.table][predict.index].ctr != WEAK_NOT_TAKEN
+       || tageTables[predict.table][predict.index].u != 0 || use_alt_on_na < USE_ALT_ON_NA_INIT)
+    {
+      predict.pred = tageTables[predict.table][predict.index].ctr > TAGE_MAX/2;
+      return predict.pred;
+    }
+    else
+    {
+      return predict.alterPred;
+    }
+  }
+  else // In case of no Hits in Tage tables(complete Miss)
+  {
+    predict.alterPred = (BHT[bimodalIndex] > BHT_MAX/2);
+    return predict.alterPred;
+  }
+
+}
+
+void train_tage(uint32_t pc, uint8_t outcome)
+{
+  // get lower ghistoryBits of pc
+  uint32_t prediction = -1;
+  uint32_t alterPrediction = -1;
+  uint32_t bimodalIndex = (pc & ((1 << BIMODAL_SIZE) - 1));
+  bool steal;
+
+  uint32_t predDir = tage_predict(pc);
+
+  if(predict.table < NUM_TAGE_TABLES) // If there is a tag Hit in Tage Tables
+  {
+    prediction = tageTables[predict.table][predict.index].ctr;
+    if(outcome && (prediction < TAGE_MAX)) tageTables[predict.table][predict.index].ctr++; // Increment the counter if prediction is TAKEN
+    else if (!outcome && (prediction > 0)) tageTables[predict.table][predict.index].ctr--; // Decrement the counter if prediction is NOT TAKEN
+
+    if(predict.alterTable != NUM_TAGE_TABLES) alterPrediction = tageTables[predict.alterTable][predict.alterIndex].ctr; // If alternate prediction exists, read the prediction counter
+
+    if(tageTables[predict.table][predict.index].u == 0 && alterPrediction != -1)
+    {
+      // Update the counters of alternate prediction
+      if(outcome && (alterPrediction < TAGE_MAX)) tageTables[predict.alterTable][predict.alterIndex].ctr++; // Increment on TAKEN
+      else if(!outcome && (alterPrediction > 0)) tageTables[predict.alterTable][predict.alterIndex].ctr--;  // Decrement on NOT TAKEN
+    }
+  }
+  else
+  {
+    prediction = BHT[bimodalIndex];
+   if(outcome && (prediction < BHT_MAX)) BHT[bimodalIndex]++; //Increment the Bimodal counter if prediction is TAKEN
+   else if(!outcome && (prediction > 0)) BHT[bimodalIndex]--; // Decrement the Bimodal counter if prediction is NOT TAKEN
+  }
+  
+  // If there is a prediction from Tage table, check for newly allocated entry in the tage table
+  if(predict.table < NUM_TAGE_TABLES)
+  {
+    if(tageTables[predict.table][predict.index].ctr == WEAK_TAKEN || tageTables[predict.table][predict.index].ctr == WEAK_NOT_TAKEN
+       || tageTables[predict.table][predict.index].u == 0)
+    {
+      if(predict.pred != predict.alterPred) // In case the predictions in prediction provider and alternate prediction are different
+      {
+	if(predict.alterPred == outcome) // If alternate prediction is correct
+	{
+	  if(use_alt_on_na < USE_ALT_ON_NA_MAX) use_alt_on_na++;
+	}
+	else if(use_alt_on_na > 0) use_alt_on_na--;
+      } 
+    }
+  }
+
+  // Updation policy on overall incorrect prediction
+  if((outcome != predDir) && (predict.table > 0)) // Steal an entry if the prediction is wrong and it doesn't use the longest history length to predict
+  {
+    steal = false;
+   for(uint32_t i = 0; i < predict.table; i++)
+   {
+     if(tageTables[i][tageIndex[i]].u == 0) steal = true; // On mis-predict, an entry which isn't useful can be allocated in bigger tage tables
+   }
+   if(!steal)
+   { // Decrement the usefulness counter for misprediction
+     for(int i = predict.table - 1; i >= 0; i--)
+     {
+       tageTables[i][tageIndex[i]].u--;
+     }
+   }
+   else
+   {
+     for(int i = predict.table - 1; i>=0 ; i--)
+     {
+       if((tageTables[i][tageIndex[i]].u == 0) && !(rand()%10)) // One in 10 times probability that the usefulness counter is 0
+       {
+         if(outcome) tageTables[i][tageIndex[i]].ctr = WEAK_TAKEN; // If branch is TAKEN, set the saturation counter to WEAK_TAKEN indicating it is a new allocation
+	 else tageTables[i][tageIndex[i]].ctr = WEAK_NOT_TAKEN; // If branch is NOT TAKEN, set the saturation counter to WEAK_NOT_TAKEN indicating it is a new allocation
+	 tageTables[i][tageIndex[i]].tag = tageTag[i]; // Allocate the new tag
+	 tageTables[i][tageIndex[i]].u = 0; // Reset the useful counter, indicating it is a new allocation and may not be a trust prediction
+	 break;
+       }
+     }
+   }
+  }
+  
+  // Update usefulness counter of predictions which are derived from the Tage tables
+  if(predict.table < NUM_TAGE_TABLES)
+  {
+    if(predict.alterPred != predDir)
+    {
+      if(outcome == predDir && tageTables[predict.table][predict.index].u < UBIT_MAX) tageTables[predict.table][predict.index].u++; // Correct prediction
+      else if(outcome != predDir && tageTables[predict.table][predict.index].u > 0) tageTables[predict.table][predict.index].u--; // Incorrect Predicion
+    }
+  }
+
+  GHR = (GHR << 1);
+  PHR = (PHR << 1);
+  if(outcome == TAKEN) GHR.set(0,1);
+  if(pc & 1) PHR = PHR + 1; // On unaligned PC
+  PHR = (PHR & ((1 << PHR_SIZE) - 1)); // Apply mask to restrict PHR to it's size
+
+  // CSR folding operation
+  for(int i = 0; i < NUM_TAGE_TABLES; i++)
+  {
+    foldReg(&csrIndex[i]);
+    foldReg(&csrTag[0][i]);
+    foldReg(&csrTag[1][i]); 
+  }
+
+  count++;
+  if(count == (1 << MAX_INSTR_COUNT))
+  {
+    count = 0; // Reset
+    if(toggle == 0) 
+    {
+      for(int i = 0; i < NUM_TAGE_TABLES; i++)
+      {
+        for(int j = 0; j < (1 << tageTableSize[j]); j++)
+        {
+          tageTables[i][j].u = tageTables[i][j].u & 2; // Mask with 10 LSB
+        }
+      }
+      toggle = 1;
+    }
+    else
+    {
+      for(int i = 0; i < NUM_TAGE_TABLES; i++)
+      {
+        for(int j = 0; j < (1 << tageTableSize[j]); j++)
+        {
+          tageTables[i][j].u = tageTables[i][j].u & 1; // Mask with 01 LSB
+        }
+      }
+      toggle = 0;
+    }
+  }
+
+}
+
+void cleanup_tage()
+{
+  free(BHT);
+  free(tageTableSize);
+  free(tageTagSize);
+  free(tageHistory);
+  free(tageIndex);
+  free(tageTag);
+  free(csrIndex);
+}
+ 
 // gshare functions
 void init_gshare()
 {
@@ -369,6 +765,7 @@ void init_predictor()
     init_tournament();
     break;
   case CUSTOM:
+    init_tage();
     break;
   default:
     break;
@@ -392,7 +789,7 @@ uint32_t make_prediction(uint32_t pc, uint32_t target, uint32_t direct)
   case TOURNAMENT:
     return tournament_predict(pc);
   case CUSTOM:
-    return NOTTAKEN;
+    return tage_predict(pc);
   default:
     break;
   }
@@ -419,7 +816,7 @@ void train_predictor(uint32_t pc, uint32_t target, uint32_t outcome, uint32_t co
     case TOURNAMENT:
       return train_tournament(pc, outcome);
     case CUSTOM:
-      return;
+      return train_tage(pc, outcome);
     default:
       break;
     }
